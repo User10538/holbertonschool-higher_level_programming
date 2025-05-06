3b7084d50c06a52a5678ee3e5b7803a09db2f887
User10538 <10538@holbertonstudents.com>
2025-05-07 01:30:54 +1000

    Fix pycodestyle error

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-def uppercase(str):    
+def uppercase(str):
     result = ""
     for i in str:
         if 'a' <= i <= 'z':
             result += "{}".format(chr(ord(i) - 32))
         else:
             result += "{}".format(i)
-    print("{}".format(result))  
+    print("{}".format(result))
