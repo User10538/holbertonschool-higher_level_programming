59ff7b684b23c8b4af1e493abfcb7926d917e808
User10538 <10538@holbertonstudents.com>
2025-05-07 01:29:34 +1000

    Fix more than 2 print

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def uppercase(str):    
+    result = ""
     for i in str:
         if 'a' <= i <= 'z':
-            print(chr(ord(i) - 32), end="")
+            result += "{}".format(chr(ord(i) - 32))
         else:
-            print(i, end="")
-    print()  
+            result += "{}".format(i)
+    print("{}".format(result))  
