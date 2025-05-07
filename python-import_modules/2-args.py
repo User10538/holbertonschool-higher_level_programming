9f3af5e91ba676306b522150b4f63d058bf95837
User10538 <10538@holbertonstudents.com>
2025-05-08 00:37:25 +1000

    find argument

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    arg_count = len(sys.argv) - 1
+    if (arg_count == 1):
+        print("1 argument: ")
+    elif (arg_count == 0):
+        print("0 argument: ")
+    else:
+        print(f"{arg_count} argument:")
+
+        for i in range(1, len(sys.argv)):
+            print(f"{i}: {sys.argv[i]}")
