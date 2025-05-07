24ebd3105571bde09e8a3f8607b5647c44175c2e
User10538 <10538@holbertonstudents.com>
2025-05-08 00:55:23 +1000

    Fix error -whitespace

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,9 +4,9 @@ import sys
 if __name__ == "__main__":
     arg_count = len(sys.argv) - 1
     if (arg_count == 0):
-        print("0 argument: ")
+        print("0 argument:")
     elif (arg_count == 1):
-        print("1 argument: ")
+        print("1 argument:")
     else:
         print(f"{arg_count} arguments:")
     for i in range(1, len(sys.argv)):
