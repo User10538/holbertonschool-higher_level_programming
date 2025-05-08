a6d35e5801ed1dd827cd428e9b42be1e20c99c3d
User10538 <10538@holbertonstudents.com>
2025-05-09 00:20:10 +1000

    Remove reduncy

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,14 +2,13 @@
 import importlib.util
 import sys
 import os
-import importlib
 
 if __name__ == "__main__":
     url = "https://github.com/hs-hq/0x02.py/raw/main/hidden_4.pyc"
     filepath = "/tmp/hidden_4.pyc"
 
     #download the file from website using curl
-    os.sys(f"curl {filepath} {url}")
+    os.system(f"curl -s -L -o {filepath} {url}")
 
     spec = importlib.util.spec_from_file_location("hidden_4", filepath)
     module = importlib.util.module_from_spec(spec)
