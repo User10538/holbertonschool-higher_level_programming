c50b942f50c238466f6458ccd7831ef562d9b87a
User10538 <10538@holbertonstudents.com>
2025-05-09 00:49:04 +1000

    Hidden

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,13 +2,12 @@
 import importlib.util
 import sys
 import os
-
 if __name__ == "__main__":
-    url = "https://github.com/hs-hq/0x02.py/raw/main/hidden_4.pyc"
-    filepath = "/tmp/hidden_4.pyc"
+    if len(sys.argv) >1 and sys.argv[1] == "bis":
+        filepath = "/tmp/hidden_4_bis.pyc"
+    else:
+        filepath = "/tmp/hidden_4.pyc"
 
-    #download the file from website using curl
-    os.system(f"curl -s -L -o {filepath} {url}")
 
     spec = importlib.util.spec_from_file_location("hidden_4", filepath)
     module = importlib.util.module_from_spec(spec)
