0a9041e9f6ce4cef10c1718ccb842d18438fe08b
User10538 <10538@holbertonstudents.com>
2025-05-09 01:18:36 +1000

    Uppercase, lowercase

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,9 +4,9 @@ def uppercase():
     result = ""
     for i in range(ord('z'), ord('a') -1, -1):
         if (ord('z') - i) % 2 == 0:
-            result += "{}".format(chr(i).lowercase())
+            result += "{}".format(chr(i).lower())
         else:
-            result += "{}".format(chr(i).uppercase())
+            result += "{}".format(chr(i).upper())
 
     print("{}".format(result))
 
