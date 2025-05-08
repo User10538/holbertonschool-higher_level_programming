9d932e2a015bacf5d78339dbde2098d147970501
User10538 <10538@holbertonstudents.com>
2025-05-09 01:11:50 +1000

    Test

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def uppercase():
+    result = ""
+    for i in range(ord('z'), ord('a') -1, -1):
+        if 'a' <= chr(i) <= 'z':
+            result += "{}".format(chr(i).upper())
+        else:
+            result += "{}".format(i)
+    print("{}".format(result))
+
+    uppercase()
