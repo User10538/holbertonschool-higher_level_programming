7e8895569205912ca84580d840b2f0f706c3f84f
User10538 <10538@holbertonstudents.com>
2025-05-08 19:19:57 +1000

    Sort names

--- python-import_modules/tmp/4-hidden_discovery.py
+++ python-import_modules/tmp/4-hidden_discovery.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+import importlib.util
+import sys
+import os
+import importlib
 
 if __name__ == "__main__":
-    import hidden_4
-    names = dir(hidden_4)
-    for name in names:
-        if not name.startswith("__")/:
-        print(name)
+    url = "https://github.com/hs-hq/0x02.py/raw/main/hidden_4.pyc"
+    filepath = "/tmp/hidden_4.pyc"
+
+    #download the file from website using curl
+    os.sys(f"curl {filepath} {url}")
+
+    spec = importlib.util.spec_from_file_location("hidden_4", filepath)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    for name in sorted(dir(module)):
+        if not name.startswith("__"):
+            print(name)
