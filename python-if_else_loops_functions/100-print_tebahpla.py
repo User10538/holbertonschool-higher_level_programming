d2fc39bc9ad5fed721197ce63cbfdc34fe4762d4
User10538 <10538@holbertonstudents.com>
2025-05-09 01:33:03 +1000

    Fix errot

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 
 def uppercase():
     result = ""
-    for i in range(ord('z'), ord('a') -1, -1):
+    for i in range(ord('z'), ord('a') - 1, -1):
         if (ord('z') - i) % 2 == 0:
             result += "{}".format(chr(i).lower())
         else:
