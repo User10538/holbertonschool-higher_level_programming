87623e72e9944caced1c6d1a04b84e3bc0556c53
User10538 <10538@holbertonstudents.com>
2025-05-12 19:58:06 +1000

    Fix error - aligment

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -5,10 +5,9 @@ def square_matrix_simple(matrix=[]):
     #loop through row in matrix#
     for row in matrix:
         square_row = []
-    
-    #loop through element
-    for x in row:
-        square_row.append(x ** 2)
-    new_matrix.append(square_row)
+        #loop through element
+        for x in row:
+            square_row.append(x ** 2)
+        new_matrix.append(square_row)
 
     return new_matrix
