408b76be2762c965550b5145aba6145cf3ddad43
User10538 <10538@holbertonstudents.com>
2025-05-12 16:19:29 +1000

    Find the largest int

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None    
+
+    largest_int = my_list[0]
+    for num in my_list:
+        if largest_int > num:
+            largest_int = num
+        return largest_int
