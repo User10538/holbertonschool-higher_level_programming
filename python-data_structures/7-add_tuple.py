cf6c8af2a19e20746ec21e661b6e5aee8b26fd1f
User10538 <10538@holbertonstudents.com>
2025-05-12 15:28:59 +1000

    FIx error - wrong arrangement

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -7,4 +7,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b_first_element = tuple_b[0] if len(tuple_b) > 0 else 0
     b_second_element = tuple_b[1] if len(tuple_b) > 1 else 0
 
-    return a_first_element + a_second_element , b_first_element + b_second_element
+    return a_first_element + b_first_element, a_second_element + b_second_element
