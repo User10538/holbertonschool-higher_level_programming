760824ecacc38e34d7628b2667b2d2d57ca321f7
User10538 <10538@holbertonstudents.com>
2025-05-12 20:35:00 +1000

    Fix pycodestyle error

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list =[]
+    new_list = []
     for i in my_list:
-        if(i == search):
-        # append is to add an item to the end of a list
+        if (i == search):
+            # append is to add an item to the end of a list
             new_list.append(replace)
         else:
             new_list.append(i)
