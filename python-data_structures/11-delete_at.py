65a8313b2b73931e9a7062e6ded077bb9e26cec8
User10538 <10538@holbertonstudents.com>
2025-05-12 18:37:23 +1000

    Delete int

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if 0 <= idx < len(my_list):
+        ### removes the item at the specified index ###
+        del my_list[idx]
+    return my_list
