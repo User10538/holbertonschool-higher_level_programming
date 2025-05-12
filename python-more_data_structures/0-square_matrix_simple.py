c31225c60ce619cecb515327ee6fb9730a30761e
User10538 <10538@holbertonstudents.com>
2025-05-12 19:53:13 +1000

    Square matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    for i in matrix:
-        for num in i:
-            print(num)
+    new_matrix = []
+    #loop through row in matrix#
+    for row in matrix:
+        square_row = []
+    
+    #loop through element
+    for x in row:
+        square_row.append(x ** 2)
+    new_matrix.append(square_row)
+
+    return new_matrix
