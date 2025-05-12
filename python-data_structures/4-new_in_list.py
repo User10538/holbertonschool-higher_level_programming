3c76071349a81afa70c8ee0a7274d710c35cc23b
User10538 <10538@holbertonstudents.com>
2025-05-12 13:00:59 +1000

    Fix error - add new_in_list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,5 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
-        my_list[idx] = element
-    return my_list
+        new_in_list = my_list.copy()
+        new_in_list[idx] = element
+        return new_in_list
+    return my_list.copy()
