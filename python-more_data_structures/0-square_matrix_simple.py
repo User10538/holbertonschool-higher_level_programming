32e0814cd8d5b4535a15001c4c4e099ca7ddeb57
User10538 <10538@holbertonstudents.com>
2025-05-12 20:08:06 +1000

    Fix pycodestyle error

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,10 +2,10 @@
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-    #loop through row in matrix#
+# loop through row in matrix
     for row in matrix:
         square_row = []
-        #loop through element
+# loop through element
         for x in row:
             square_row.append(x ** 2)
         new_matrix.append(square_row)
