53cdd15ff6f8fea6b6fdc4f3be4c4b4eee7faf03
User10538 <10538@holbertonstudents.com>
2025-05-12 18:39:33 +1000

    Fix pycodestyle error

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,6 +2,6 @@
 
 def delete_at(my_list=[], idx=0):
     if 0 <= idx < len(my_list):
-        ### removes the item at the specified index ###
+        # removes the item at the specified index #
         del my_list[idx]
     return my_list
