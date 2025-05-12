5eef01772d2a7623e0c9538aaf4a7f748ea94356
User10538 <10538@holbertonstudents.com>
2025-05-12 15:26:18 +1000

    FIx error

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,10 +3,8 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a_first_element = tuple_a[0] if len(tuple_a) > 0 else 0
     a_second_element = tuple_a[1] if len(tuple_a) > 1 else 0
-    
+
     b_first_element = tuple_b[0] if len(tuple_b) > 0 else 0
     b_second_element = tuple_b[1] if len(tuple_b) > 1 else 0
 
-    result = a_first_element + a_second_element + b_first_element + b_second_element
-
-    return result
+    return a_first_element + a_second_element , b_first_element + b_second_element
