1c0eaf96456cac2ea9ef0d180d5d8eec8516f830
User10538 <10538@holbertonstudents.com>
2025-05-12 15:31:29 +1000

    FIx pycodestyle

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    a_first_element = tuple_a[0] if len(tuple_a) > 0 else 0
-    a_second_element = tuple_a[1] if len(tuple_a) > 1 else 0
+    a_first = tuple_a[0] if len(tuple_a) > 0 else 0
+    a_second = tuple_a[1] if len(tuple_a) > 1 else 0
 
-    b_first_element = tuple_b[0] if len(tuple_b) > 0 else 0
-    b_second_element = tuple_b[1] if len(tuple_b) > 1 else 0
+    b_first = tuple_b[0] if len(tuple_b) > 0 else 0
+    b_second = tuple_b[1] if len(tuple_b) > 1 else 0
 
-    return a_first_element + b_first_element, a_second_element + b_second_element
+    return a_first + b_first, a_second + b_second
