15e70d719952e0d72c7d9d743a31b37b583f066f
User10538 <10538@holbertonstudents.com>
2025-05-13 15:20:13 +1000

    Fix error

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for i in a_dictionary:
-        print(f"{i}".format(a_dictionary[i]))
+    for i in sorted(a_dictionary):
+        print("{}: {}".format(i, a_dictionary[i]))
