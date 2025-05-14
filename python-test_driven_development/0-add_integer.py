24d5bbb03262398dd854024a7fd94638a7dc6a32
User10538 <10538@holbertonstudents.com>
2025-05-15 00:01:23 +1000

    Add int

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    result = None
+    result = int(a) + int(b)
+    if not isinstance(a, (float, int)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b,(float, int)):
+        raise TypeError("b must be an integer")
+    return result
