ee838637912c956fda4d89f791436849d5c90933
User10538 <10538@holbertonstudents.com>
2025-05-14 22:50:49 +1000

    Fix error

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
+    
     for i in range(list_length):
+        result = 0
         try:
-            a = my_list_1[i]
-            b = my_list_2[i]
-            result = a / b
+            new_list.apprend(my_list_1[i] / my_list_2[i])
         except(TypeError):
             print("wrong type")
         except(ZeroDivisionError):
