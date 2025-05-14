18d77e1f9af06f9cb3b5824784881cce46d17b81
User10538 <10538@holbertonstudents.com>
2025-05-15 00:15:34 +1000

    Fix pycodestyle error

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,11 +4,14 @@ This module is to add two integers.
 
 The function adds a and b=98.
 """
+
+
 def add_integer(a, b=98):
+
     result = None
     result = int(a) + int(b)
     if not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
-    if not isinstance(b,(float, int)):
+    if not isinstance(b, (float, int)):
         raise TypeError("b must be an integer")
     return result
