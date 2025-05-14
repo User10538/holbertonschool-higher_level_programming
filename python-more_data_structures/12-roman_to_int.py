f45b602d90e6aff2b48272bf419bebd2ee76c0e9
User10538 <10538@holbertonstudents.com>
2025-05-14 15:17:02 +1000

    Fix error- string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or roman_string == "":
+    if not isinstance(roman_string, str) or roman_string == "":
         return 0
     roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
     sum = 0
