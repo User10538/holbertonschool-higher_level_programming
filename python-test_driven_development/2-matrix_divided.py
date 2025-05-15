c76002a59887793f60cfcc1049c3892454182c4c
User10538 <10538@holbertonstudents.com>
2025-05-15 17:41:50 +1000

    Fix regular tests

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -34,13 +34,13 @@ def matrix_divided(matrix, div):
 
     if type(div) not in (float, int):
         raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    
     for row in matrix:
         new_row = []
         for i in row:
             new_row.append(round(i/div, 2))
         result.append(new_row)
-
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
 
     return result
