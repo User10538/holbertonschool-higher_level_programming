1176fa045f0fce01cc63e6519a7d2db6e7741360
User10538 <10538@holbertonstudents.com>
2025-05-15 15:53:45 +1000

    Fix error - len of the matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -21,20 +21,21 @@ def matrix_divided(matrix, div):
 
     result = []
     for row in matrix:
-        new_row =  []
+        new_row = []
         for i in row:
             new_row.append(round(i/div, 2))
         result.append(new_row)
-        
+
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
-    #if not isinstance(matrix, (float, int)):
-        #raise TypeError("Each row of the matrix must have the same size")
+        raise TypeError("matrix must be a matrix"
+                        "(list of lists) of integers/floats")
+
+    if len(row) != len(matrix[0]):
+        raise TypeError("Each row of the matrix must have the same size")
 
     if type(div) not in (float, int):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-        
+
     return result
