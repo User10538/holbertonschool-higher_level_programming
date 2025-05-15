740eff4ae5a33e82039db74b86547c39d393edcd
User10538 <10538@holbertonstudents.com>
2025-05-15 17:30:10 +1000

    Fix spacing

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -27,7 +27,7 @@ def matrix_divided(matrix, div):
                         " (list of lists) of integers/floats")
     if not all(isinstance(i, (int, float)) for row in matrix for i in row):
         raise TypeError("matrix must be a matrix"
-                        "(list of lists) of integers/floats")
+                        " (list of lists) of integers/floats")
 
     if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
