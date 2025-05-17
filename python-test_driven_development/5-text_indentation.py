572624fdff0f913904e9c1352538c238e1d096b9
User10538 <10538@holbertonstudents.com>
2025-05-17 22:09:41 +1000

    Add more regular tests

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -26,7 +26,10 @@ def text_indentation(text):
     start = 0
     for i, c in enumerate(text):
         if c in ".?:":
-            print(text[start:i + 1].strip(), end="\n\n")
+            print(text[start:i + 1].strip())
             start = i + 1
+            while start < len(text) and text[start] == '':
+                start += 1
+                
     if start < len(text):
         print(text[start:].strip(), end="")
