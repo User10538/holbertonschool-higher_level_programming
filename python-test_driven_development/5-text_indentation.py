2033dd7a4e4cdb932d7405cdf50975a57c93b075
User10538 <10538@holbertonstudents.com>
2025-05-17 22:48:11 +1000

    Fix blank lines

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -22,11 +22,11 @@ def text_indentation(text):
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
+    
     start = 0
     for i, c in enumerate(text):
         if c in ".?:":
-            print(text[start:i + 1].strip(), end="\n")
+            print(text[start:i + 1].strip(), end="\n\n")
             start = i + 1
     if start < len(text):
         print(text[start:].strip(), end="")
