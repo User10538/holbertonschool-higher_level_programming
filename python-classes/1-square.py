e176a5944c30873b094956a02bbe13b4eca288ae
User10538 <10538@holbertonstudents.com>
2025-05-19 16:03:17 +1000

    Private attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Write class Square that defines a square by 
+Private instance attribute: size
+Instantiation with size
+"""
+
+
+class Square:
+    """
+    A square class with private instance attribute: size
+    Instantiation with size
+    """
+
+    def __init__(self, size):
+        self.__size = size # this is private attrribute
