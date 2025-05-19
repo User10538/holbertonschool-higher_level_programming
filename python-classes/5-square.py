1bd8c1967c8011af3c35548e04c8f73c2cf4b025
User10538 <10538@holbertonstudents.com>
2025-05-19 17:44:14 +1000

    Print #

---
+++ python-classes/5-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Write class Square that defines a square by
+Private instance attribute: size
+Instantiation with optional size: def __init__(self, size=0):
+
+Public instance method: def my_print(self): that prints in stdout
+the square with the character #:
+if size is equal to 0, print an empty line
+"""
+
+
+class Square:
+    """
+    A square class with Private instance attribute: size
+    Instantiation with optional size: def __init__(self, size=0):
+    size must be an integer, otherwise raise a TypeError exception
+    with the message size must be an integer
+    if size is less than 0, raise a ValueError exception with the
+    message size must be >= 0
+
+    New addition - Public instance method: def my_print(self):
+    that prints in stdout the square with the character #:
+    if size is equal to 0, print an empty line
+    """
+
+    def __init__(self, size=0):
+        self.size = size  # Triggers the setter (validation)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Returns the current square area
+        """
+
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print('#' * self.size)
