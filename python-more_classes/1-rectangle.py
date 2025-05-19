b3a61706b29eb2b26f4c15ae32d62b3cd579f552
User10538 <10538@holbertonstudents.com>
2025-05-19 19:25:44 +1000

    Fix error - in setter

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Write an empty class Rectangle that defines a rectangle
+Write an class Rectangle that defines a rectangle with width and height
 """
 
 
 class Rectangle:
     """
-    Write an empty class Rectangle that defines a rectangle
+    Write an class Rectangle that defines a rectangle with width and height
 
     """
 
@@ -24,6 +24,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -35,3 +36,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
