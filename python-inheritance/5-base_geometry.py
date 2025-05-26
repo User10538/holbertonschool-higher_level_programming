b0aa60a0e0480bb145b0f980b8a91305fe2a320a
User10538 <10538@holbertonstudents.com>
2025-05-26 12:53:20 +1000

    Empty class with pass

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Write a function that returns True if the object is an instance of
+a class that inherited (directly or indirectly) from
+the specified class ; otherwise False.
+"""
+class BaseGeometry:
+    pass
