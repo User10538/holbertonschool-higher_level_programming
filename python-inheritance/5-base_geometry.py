5de1c977d57a0328edc4f83358e1eaa0bcbb0834
User10538 <10538@holbertonstudents.com>
2025-05-26 13:02:43 +1000

    Fix error

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -10,4 +10,4 @@ class BaseGeometry:
     Raises an Exception with the message area() is not implemented.
     """
     def area(self):
-        raise Exception ("area() is not implemented")
+        raise Exception("area() is not implemented")
