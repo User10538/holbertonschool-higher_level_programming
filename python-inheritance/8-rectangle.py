5f5346d3d415f1907db1162615a3941c4798d3a9
User10538 <10538@holbertonstudents.com>
2025-05-26 14:47:58 +1000

    Add BaseGeometry into Rectangle class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """
-Write a class BaseGeometry (based on 6-base_geometry.py).
-Raises an Exception with the message area() is not implemented
-Raise a TypeError exception, with the message <name> must be an integer
-Raise a ValueError exception with the message <name> must be greater than 0
+Write a class Rectangle that inherits from
+BaseGeometry (7-base_geometry.py).
 """
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class Rectangle:
+class Rectangle(BaseGeometry):
     """
     Write a class Rectangle that inherits from
     BaseGeometry (7-base_geometry.py).
