8f1429d0dde0f061b46587b893f3a35aaa9b120c
User10538 <10538@holbertonstudents.com>
2025-05-26 15:58:38 +1000

    Get square that inherit from rect

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -17,7 +17,10 @@ class Square(Rectangle):
 
     """
     def __init__(self, size):
-        self.integer_validator(size)
+        self.integer_validator(size, size)
+        super().__init__(size, size)
         self.__size = size
 
-        self.area
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
