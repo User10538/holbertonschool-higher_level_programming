d8f8ae039b868f00fc882c8253ea40ac26e7078a
User10538 <10538@holbertonstudents.com>
2025-05-26 14:49:32 +1000

    Fix pycodestyle error

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,11 +7,13 @@ BaseGeometry (7-base_geometry.py).
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
+
     """
     Write a class Rectangle that inherits from
     BaseGeometry (7-base_geometry.py).
-    
+
     """
     def __init__(self, width, height):
         self.integer_validator("width", width)
