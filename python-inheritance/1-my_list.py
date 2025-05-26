e4ff5ed569f616d47e4d1fc03a1a6c445e9decc8
User10538 <10538@holbertonstudents.com>
2025-05-26 11:32:09 +1000

    Print list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a class MyList that inherits from list:
+Prints the list, but sorted (ascending sort)
+all the elements of the list will be of type int
+"""
+
+
+def print_sorted(self): 
+    """
+    Write a class MyList that inherits from list sorted (ascending sort)    
+    """
+    print sorted(self)
