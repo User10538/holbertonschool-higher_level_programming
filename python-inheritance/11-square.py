08d3cbf7aa4e06fb3ac339d2d8fae1b8735ed88d
User10538 <10538@holbertonstudents.com>
2025-05-26 16:21:58 +1000

    Add Str

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Write a class Square that inherits from Rectangle (9-rectangle.py):
+
+size must be a positive integer, validated by integer_validator
+the area() method must be implemented
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    """
+    Write a class Square that inherits from Rectangle (9-rectangle.py):
+
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        # Area = side × side
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
