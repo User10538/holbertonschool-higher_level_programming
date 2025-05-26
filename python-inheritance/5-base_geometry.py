4eecff356e0659a7bc8d36d45338c08e6582b47a
User10538 <10538@holbertonstudents.com>
2025-05-26 13:00:31 +1000

    Add exception

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-Write an empty class BaseGeometry.
+Write a class BaseGeometry (based on 5-base_geometry.py).
 """
 
 
 class BaseGeometry:
     """
-    Write an empty class BaseGeometry.
+    Write a class BaseGeometry (based on 5-base_geometry.py).
+    Raises an Exception with the message area() is not implemented.
     """
-    pass
+    def area(self):
+        raise Exception ("area() is not implemented")
