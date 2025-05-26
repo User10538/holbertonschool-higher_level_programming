7b739ee379190943ffb7a6bec9a1105e18b92953
User10538 <10538@holbertonstudents.com>
2025-05-26 15:47:04 +1000

    Fix pycodestyle error

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,12 +11,12 @@ class BaseGeometry:
     """
     Write a class BaseGeometry (based on 6-base_geometry.py).
     Raises an Exception with the message area() is not implemented
-    
+
     """
     def area(self):
 
         raise Exception("area() is not implemented")
-    
+
     """
     Raise a TypeError exception, with the message <name> must be an integer
     Raise a ValueError exception with the message <name> must be greater than 0
@@ -25,5 +25,5 @@ class BaseGeometry:
 
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
-        if value <= 0: 
+        if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
