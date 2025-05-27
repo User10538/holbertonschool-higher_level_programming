3d978a6d88b7fb8f2e77495f74c12ace770cf20e
User10538 <10538@holbertonstudents.com>
2025-05-27 15:19:56 +1000

    Fix error - pop error

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -8,17 +8,17 @@ class VerboseList(list):
     """
     def append(self, item):
         super().append(item)
-        print(f"Added {item} to the list.")
+        print(f"Added {[item]} to the list.")
 
     def extend(self, items):
         super().extend(items)
-        print(f"Extended the list with {len(items)} items.")
+        print(f"Extended the list with {len([items])} items.")
 
     def remove(self, item):
         super().remove(item)
-        print(f"Removed {item} from the list.")
+        print(f"Removed {[item]} from the list.")
 
-    def pop(self, item=-1):
-        super().pop(item)
-        print(f"Popped {item} from the list.")
+    def pop(self, index=-1):
+        item = super().pop(index)
+        print(f"Popped {[index]} from the list.")
         return item
