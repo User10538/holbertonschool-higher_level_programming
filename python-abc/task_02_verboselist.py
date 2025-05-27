e3e0df4553200b3bccc9f195a7ad2ef5e9e436e4
User10538 <10538@holbertonstudents.com>
2025-05-27 15:17:03 +1000

    class that extends python list class

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+class VerboseList(list):
+    """
+    Create a class named VerboseList that extends the
+    Python list class. This custom class should
+    print a notification message every time an item is added or removed.
+    """
+    def append(self, item):
+        super().append(item)
+        print(f"Added {item} to the list.")
+
+    def extend(self, items):
+        super().extend(items)
+        print(f"Extended the list with {len(items)} items.")
+
+    def remove(self, item):
+        super().remove(item)
+        print(f"Removed {item} from the list.")
+
+    def pop(self, item=-1):
+        super().pop(item)
+        print(f"Popped {item} from the list.")
+        return item
