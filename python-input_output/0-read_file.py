3b725f29e68e98c7e2a913d1d66963a3e2fbf043
User10538 <10538@holbertonstudents.com>
2025-06-02 17:00:52 +1000

    Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+
+        # We can check that the file has been automatically closed.
+        f.closed
+        True
