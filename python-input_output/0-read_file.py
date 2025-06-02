4afc5184b10699fc4645ec71ea7952df341089d0
User10538 <10538@holbertonstudents.com>
2025-06-02 17:03:59 +1000

    Fix pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+Write a function that reads a text file (UTF8) and prints it to stdout:
+
+"""
+
 
 def read_file(filename=""):
+    """
+    Must use the with statement.
+
+    Don’t need to manage file permission or file doesn't exist exceptions.
+    """
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
 
