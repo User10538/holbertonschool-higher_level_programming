351dadc86f91124d9cb76f024797511f8dbc67e5
User10538 <10538@holbertonstudents.com>
2025-06-03 10:39:10 +1000

    Write text to file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a function that writes a string to a text file (UTF8) and returns the number of characters written:
+
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Must use the with statement.
+
+    Don’t need to manage file permission or file doesn't exist exceptions.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
