b35b529337907381b7b4576b8a6d13226405eb53
User10538 <10538@holbertonstudents.com>
2025-06-03 21:25:25 +1000

    Change to json

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -3,10 +3,10 @@ import json
 
 def serialize_and_save_to_file(data, filename):
     # Your code here to serialize and save data to the specified file
-    with open(filename, "wb") as f:
+    with open(filename,"w", encoding="utf-8") as f:
         json.dump(data, f)
 
 def load_and_deserialize(filename):
     # Your code here to load and deserialize data from the specified file
-    with open(filename, "rb") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
