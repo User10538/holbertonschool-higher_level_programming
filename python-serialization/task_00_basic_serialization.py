0229876194a29f3f3886041ac2b0cf6258b6c8b4
User10538 <10538@holbertonstudents.com>
2025-06-03 21:23:14 +1000

    Change to json

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import pickle
+import json
 
 def serialize_and_save_to_file(data, filename):
     # Your code here to serialize and save data to the specified file
     with open(filename, "wb") as f:
-        pickle.dump(data, f)
+        json.dump(data, f)
 
 def load_and_deserialize(filename):
     # Your code here to load and deserialize data from the specified file
     with open(filename, "rb") as f:
-        return pickle.load(f)
+        return json.load(f)
