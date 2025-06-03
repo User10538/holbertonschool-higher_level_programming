97b5cc8b7715a6c4c01091eb1815fa7a982828e6
User10538 <10538@holbertonstudents.com>
2025-06-03 21:20:58 +1000

    Add test non serializable object

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,10 +5,8 @@ def serialize_and_save_to_file(data, filename):
     # Your code here to serialize and save data to the specified file
     with open(filename, "wb") as f:
         pickle.dump(data, f)
-    pass
 
 def load_and_deserialize(filename):
     # Your code here to load and deserialize data from the specified file
     with open(filename, "rb") as f:
         return pickle.load(f)
-    pass
