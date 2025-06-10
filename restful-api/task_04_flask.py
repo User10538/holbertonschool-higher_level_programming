57fdf3124bde4ff7e90fbec208f47b48ca3bbf0d
User10538 <10538@holbertonstudents.com>
2025-06-10 19:03:53 +1000

    Using Flask

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import json, requests
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
+
+@app.route('/')
+def home():
+    return 'Welcome to the Flask API!'
+
+@app.route('/data')
+def data():
+    return jsonify(users)
+
+@app.route('/status')
+def status():
+    return 'OK'
+
+@app.route('/users/<username>')
+def GetUsers(username):
+    if username in users:
+        return jsonify(users[username])
+
+@app.route('/add_user', methods = ['POST'])
+def AddUsers():
+    #data = {"username": "john", "name": "John", "age": 30, "city": "New York"}
+    data = requests.get_json()
+
+    username = data['username']
+
+    users[username] = {"name": data.get("name", ""), 
+                       "age": data.get("age", 0),
+                       "city": data.get("city", "")}
+    return jsonify(users[username])
+
+
+    
+
+if __name__ == "__main__": 
+    app.run(port=8000)
