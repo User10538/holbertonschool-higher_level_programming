867684d824ba01f9dbcddeaae88e738beb9d883b
User10538 <10538@holbertonstudents.com>
2025-06-10 17:48:01 +1000

    Add more self to for self path

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -10,10 +10,15 @@ class MySimpleHandle(BaseHTTPRequestHandler):
             dataSet = {"name": "John", "age": 30, "city": "New York"}
             jsonData = json.dumps(dataSet).encode('utf-8')
 
+            self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             self.wfile.write(jsonData)
-        else:
+
+        elif self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
 
 def run(server_class=HTTPServer, handler_class=MySimpleHandle):
