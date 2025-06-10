142d5978ab74de89dcf651ef3f57a69a077ec089
User10538 <10538@holbertonstudents.com>
2025-06-10 17:41:49 +1000

    Changed handler class

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -3,21 +3,24 @@ import json
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('', 8000)
-    httpd = server_class(server_address, handler_class)
-    print(f"Starting server..")
-    httpd.serve_forever()
-
-class mySimpleHandle(BaseHTTPRequestHandler):
+class MySimpleHandle(BaseHTTPRequestHandler):
 
     def do_GET(self):
         if self.path == "/data":
             dataSet = {"name": "John", "age": 30, "city": "New York"}
-            jsonData = json.dumps(dataSet).encode('utd-8')
+            jsonData = json.dumps(dataSet).encode('utf-8')
 
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             self.wfile.write(jsonData)
         else:
-            self.wfile.write("Hello, this is a simple API!")
+            self.wfile.write(b"Hello, this is a simple API!")
+
+def run(server_class=HTTPServer, handler_class=MySimpleHandle):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting server..")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
