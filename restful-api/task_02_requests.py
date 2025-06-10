888de816232d0686e46509c13cdd0c6cda7fb4ec
User10538 <10538@holbertonstudents.com>
2025-06-10 10:35:31 +1000

    Test code

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 import json
 import requests
+import csv
 
 url = "https://jsonplaceholder.typicode.com/posts"
 
-def fetch_and_print_posts(self):
+def fetch_and_print_posts():
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
         for post in data:
             print(post['title'])
+    else: 
+        print(f"Failed to fetch post. Status code: {response.status_code}")
 
-def fetch_and_save_posts(self):
+def fetch_and_save_posts():
     r = requests.get()
     r.json()
