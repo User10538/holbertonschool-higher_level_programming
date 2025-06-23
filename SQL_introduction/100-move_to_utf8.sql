fab013bd0e72d56e263c971a95991c7aa7b7da01
User10538 <10538@holbertonstudents.com>
2025-06-23 19:08:18 +1000

    Fix error -USE hbtn_0c_0;

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,13 +1,12 @@
 USE hbtn_0c_0;
 
--- Convert the database character set and collation
+-- Convert database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
--- Convert the table character set and collation
+-- Convert table
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Ensure the 'name' column is using the correct charset and collation
+-- Modify 'name' field without setting CHARACTER SET (only COLLATE)
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
