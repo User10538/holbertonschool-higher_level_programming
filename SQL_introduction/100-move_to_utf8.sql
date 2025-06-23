e0f9ac52864e3898d6a8cb9c66b576a0fc9ec51c
User10538 <10538@holbertonstudents.com>
2025-06-23 19:06:01 +1000

    Move to utf8mb4

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,11 @@
+-- Convert the database character set and collation
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Convert the table character set and collation
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Ensure the 'name' column is using the correct charset and collation
+ALTER TABLE first_table
+MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
