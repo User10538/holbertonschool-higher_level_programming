ebd3b56a688a5fe9895ca0b12ae75b54c4f3b096
User10538 <10538@holbertonstudents.com>
2025-06-23 19:11:42 +1000

    Fix error

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,12 @@
 -- Set the database charset and collation
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Use the database
+-- Select the database
 USE hbtn_0c_0;
 
--- Convert the table
+-- Convert the table to utf8mb4
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Remove column-level charset from `name` field
+-- Redefine the column, removing explicit charset
 ALTER TABLE first_table
 MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
