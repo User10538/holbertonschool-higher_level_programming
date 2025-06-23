1feaa17b03326110300d0afcb6f305dac04aa5ce
User10538 <10538@holbertonstudents.com>
2025-06-23 20:08:39 +1000

    Create and grant access to user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- This script creates the MySQL server user user_0d_1.
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
