9dc837594b99c197009fbe4919a215cb440eccb2
User10538 <10538@holbertonstudents.com>
2025-06-23 20:10:55 +1000

    Add privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to the user
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
