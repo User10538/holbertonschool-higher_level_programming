afedabfd13866f7a95708f7bb1f88c08a754a8bc
User10538 <10538@holbertonstudents.com>
2025-06-23 23:03:06 +1000

    Create db and user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- This script create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- This script creates the MySQL server user user_0d_2.
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant select privileges to the user
+GRANT SELECT PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
