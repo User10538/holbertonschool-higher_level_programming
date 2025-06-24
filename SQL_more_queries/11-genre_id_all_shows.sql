0a1387645330c6675cc26d4f30fa5e8d77844887
User10538 <10538@holbertonstudents.com>
2025-06-24 18:01:21 +1000

    Fix error

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 -- This script import the database dump of hbtn_0d_tvshows to your MySQL server
 
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres 
-WHERE tv_show_genres.show_id = tv_shows.id 
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC LIMIT 1 OFFSET 0;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
