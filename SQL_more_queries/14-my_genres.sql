e56b1cf99ebf34d17e9de4d406682599b42c87f6
User10538 <10538@holbertonstudents.com>
2025-06-24 20:14:18 +1000

    Select name from tv shows

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,4 @@
+-- This script import the database dump from hbtn_0d_tvshows to your MySQL
+
+SELECT tv_genres.name FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE title = 'Dexter' ORDER BY tv_genres.name ASC
