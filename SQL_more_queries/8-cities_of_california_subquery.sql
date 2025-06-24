68f74a132b75c5d5d63fc7d6717d90b2bc4a1fcd
User10538 <10538@holbertonstudents.com>
2025-06-24 16:05:11 +1000

    Fix error

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,4 @@
 -- This script  lists all the cities of California that can be found in the database 
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states table WHERE name = California)ORDER BY cities.id ASC
+SELECT id, name FROM cities WHERE 
+state_id = (SELECT id FROM states table WHERE name = 'California') 
+ORDER BY cities.id ASC
