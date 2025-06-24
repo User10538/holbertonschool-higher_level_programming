dede97491e4f3ec44b8d0f6501e525ec99b813c8
User10538 <10538@holbertonstudents.com>
2025-06-24 19:43:52 +1000

    import db dump

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,4 @@
+-- This script Import the database dump from hbtn_0d_tvshows to your MySQL
+SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM genres LEFT JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.name ORDER BY  number_of_shows DESC;
