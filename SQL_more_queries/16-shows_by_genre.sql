1fc5401773a9ca4e58e405b61e8cddc60efa9698
User10538 <10538@holbertonstudents.com>
2025-06-24 20:54:37 +1000

    left join INSTEAD OF inner join

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,3 @@
 -- This script import the database dump from hbtn_0d_tvshows to your MySQL server
-SELECT tv_shows.title, tv_genres.name FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id ORDER BY tv_shows.title ASC, tv_genres.name ASC
+SELECT tv_shows.title, tv_genres.name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id ORDER BY tv_shows.title ASC, tv_genres.name ASC
