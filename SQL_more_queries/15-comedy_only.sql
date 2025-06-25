3ad75b41f6f1a8a3680c029e48fb5077e7499b90
User10538 <10538@holbertonstudents.com>
2025-06-25 11:00:32 +1000

    Upload to save

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,7 @@
 -- This script Import the database dump from hbtn_0d_tvshows to your MySQL server
-SELECT tv_shows.title FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE name = 'Comedy' ORDER BY tv_shows.title ASC
+SELECT tv_shows.title 
+FROM tv_shows 
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id 
+WHERE name = 'Comedy' 
+ORDER BY tv_shows.title ASC
--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
 -- This script  lists all the cities of California that can be found in the database 
-SELECT id, name FROM cities WHERE 
-state_id = (SELECT id FROM states WHERE name = 'California') 
+SELECT id, name 
+FROM cities 
+WHERE state_id = (SELECT id FROM states WHERE name = 'California') 
 ORDER BY cities.id ASC
