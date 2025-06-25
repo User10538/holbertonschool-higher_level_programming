ff085919fbe3113ca9349d3f3b81f41c6cd735eb
User10538 <10538@holbertonstudents.com>
2025-06-25 11:52:53 +1000

    Add modules

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
+"""
 
+Your script should take 3 arguments: mysql username, mysql password
+and database name (no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    
-    username = sys.argv[1] 
+
+    username = sys.argv[1]
     password = sys.argv[2]
     name = sys.argv[3]
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=name, charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=name, charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
