d7f8a088085601e2bbd6ad05d82189365d60d419
User10538 <10538@holbertonstudents.com>
2025-06-25 13:37:45 +1000

    Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+
+Your script should take 3 arguments: mysql username, mysql password
+and database name (no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=name, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
