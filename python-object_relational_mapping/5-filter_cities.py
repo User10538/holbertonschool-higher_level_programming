f2f64a946c1b54aca245c0dfd020c258e4a5c1ea
User10538 <10538@holbertonstudents.com>
2025-06-30 01:25:54 +1000

    Filter cities

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Lists all cities in the database hbtn_0e_4_usa
+Takes 3 arguments: mysql username, mysql password, and database name.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit("Usage: ./script.py <username> <password> <database>")
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
+    cur = db.cursor()
+
+    # Execute single query to get cities and their states,
+    # ordered by city id ascending
+    query = """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """
+    cur.execute(query, (state_name,))
+
+    # Print each row as a tuple exactly as requested
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
