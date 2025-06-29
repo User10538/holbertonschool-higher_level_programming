33578f479c802a002807fe9ff18cbc150156d4c7
User10538 <10538@holbertonstudents.com>
2025-06-30 00:39:22 +1000

    Safe SQL

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Lists all values in the `states` table where name matches the argument.
+Takes 4 arguments: mysql username, mysql password,
+database name, and state name.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    searched = sys.argv[4]
+
+    # Establish connection
+    conn = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=username, passwd=password,
+        db=db_name, charset="utf8"
+    )
+    cur = conn.cursor()
+
+    # Sanitize single quotes in input (very basic protection)
+    searched_safe = searched.replace("'", "''")
+
+    # Proper query construction with format()
+    query = (
+        "SELECT * FROM states "
+        "WHERE BINARY name = '{}' ORDER BY id ASC"
+    ),(searched_safe,)
+
+    cur.execute(query)
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    conn.close()
