d2b830eb035beb15432371074e11ed8e4e653399
User10538 <10538@holbertonstudents.com>
2025-06-30 12:46:00 +1000

    Fix error

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 import sys
 from model_state import Base, State
@@ -9,7 +9,19 @@ from sqlalchemy.orm import Session
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}', pool_pre_ping=True)
+    if len(sys.argv) != 5:
+        sys.exit("Usage: ./script.py <username> <password> "
+                 "<database> <state_name>")
+    
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+        
+    engine = create_engine(
+    "mysql+mysqldb://{}:{}@localhost:3306/{}".format(username, password, db_name),
+    pool_pre_ping=True
+)
+
     Base.metadata.create_all(engine)
 
     session = Session(engine)
