23417207174f69559974f0f7c686d85caf5f8ea7
User10538 <10538@holbertonstudents.com>
2025-06-30 13:52:18 +1000

    State with an A

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit("Usage: ./script.py <username> <password> "
+                 "<database> <state_name>")
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(username, password, db_name),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    
+    states_with_a = session.query(State)\
+        .filter(State.name.like('%a%'))\
+        .order_by(State.id)\
+        .all()
+
+    if states_with_a:
+        for state in states_with_a:
+            print(f"{state.id}: {state.name}")
+    else:
+        print(f"Nothing")
+    session.close()
