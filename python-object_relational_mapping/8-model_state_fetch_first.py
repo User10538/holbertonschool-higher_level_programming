1e2cbc1cc0972405abc79a2c81cd1c5d8aa7ae5b
User10538 <10538@holbertonstudents.com>
2025-06-30 13:42:30 +1000

    Print first state

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit("Usage: ./script.py <username> <password> "
+                 "<database> <state_name>")
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(username, password, db_name),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    # Fetch the first State object ordered by id
+    first_state = session.query(State).order_by(State.id).first()
+    
+    # HERE: no SQL query, only objects!
+    for state in session.query(State).order_by(State.id).all():
+        print(f"{first_state.id}: {first_state.name}")
+    session.close()
