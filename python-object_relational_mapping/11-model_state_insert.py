26f2be2629b3346d8b4242cac4b7e035b37baa97
User10538 <10538@holbertonstudents.com>
2025-06-30 14:42:29 +1000

    Add new state

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit("Usage: ./script.py <username> <password> "
+                 "<database>")
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(username, password, db_name),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    # Add new state 
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)    
+
+    session.close()
