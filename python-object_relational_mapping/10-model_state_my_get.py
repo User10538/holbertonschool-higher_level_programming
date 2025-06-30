cb4f5fd56864a1088b599341425ef96e962a74b7
User10538 <10538@holbertonstudents.com>
2025-06-30 14:08:38 +1000

    Get searched results

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit("Usage: ./script.py <username> <password> "
+                 "<database> <search>")
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    search = sys.argv[4]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(username, password, db_name),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    # Query for states whose name contains the search string
+    results = session.query(State).filter(State.name.like(f"%{search}%")).order_by(State.id).all()
+    
+    if results:
+        for state in results:
+            print(f"{state.id}")
+    else:
+        print(f"Not found")
+
+    session.close()
