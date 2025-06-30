778ab356ce76ebfdb57a578eba3ad8e082b5f0b7
User10538 <10538@holbertonstudents.com>
2025-06-30 15:06:05 +1000

    Update states

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit("Usage: ./script.py <username> <password> "
+                 "<database>")
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(username, password, db_name),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    # Update state
+    update_state = session.query(State)\
+        .filter(State.id==2).first()
+    if update_state:
+        update_state.name = 'New Mexico'
+        session.commit()
+
+    session.close()
