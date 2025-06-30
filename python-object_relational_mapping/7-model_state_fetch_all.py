654f4508838cc5733b7d10af30dd10199d858df1
User10538 <10538@holbertonstudents.com>
2025-06-30 12:52:06 +1000

    Fix error

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,19 +12,20 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         sys.exit("Usage: ./script.py <username> <password> "
                  "<database> <state_name>")
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-        
+
     engine = create_engine(
-    "mysql+mysqldb://{}:{}@localhost:3306/{}".format(username, password, db_name),
-    pool_pre_ping=True
-)
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(username, password, db_name),
+        pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).all(): # HERE: no SQL query, only objects!
+    # HERE: no SQL query, only objects!
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
