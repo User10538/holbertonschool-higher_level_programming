11772519ea427a4fb86205fd369757a896821873
User10538 <10538@holbertonstudents.com>
2025-06-30 11:38:23 +1000

    Fix pycodestyle error

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,11 +11,11 @@ import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-
-""" 
+"""
 Create the base class for class definitions
 """
 Base = declarative_base()
+
 
 class State(Base):
     """
