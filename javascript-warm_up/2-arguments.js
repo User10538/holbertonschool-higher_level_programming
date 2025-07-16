63c8559d445e80af1b1ab0f1d315550ad5c2678e
User10538 <10538@holbertonstudents.com>
2025-07-16 13:24:33 +1000

    Argument

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function checkForArgument (arg) {
+  if (arg.length === 0) {
+    console.log('No argument');
+  } else if (arg.length === 1) {
+    console.log('Argument found');
+  } else {
+    console.log('Argument found');
+  }
+}
+
+// Example usage:
+checkForArgument(); // No argument
+checkForArgument('test'); // Argument found
+checkForArgument(1, 2, 3); // Arguments found
