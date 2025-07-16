4e181dbbcc9b710ccc7ca104d29105096f458646
User10538 <10538@holbertonstudents.com>
2025-07-16 16:41:17 +1000

    5. An Integer

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -4,6 +4,7 @@ const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
 if (arg1 === undefined || arg2 === undefined) {
+    
   const failed = String(arg1).concat(' is ', String(arg2));
   console.log(failed);
 } else {
