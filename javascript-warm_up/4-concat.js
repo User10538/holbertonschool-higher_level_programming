53032843cf8fd7fb248d2716a85a643b81443c84
User10538 <10538@holbertonstudents.com>
2025-07-16 16:25:08 +1000

    Fix error

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (arg1 === undefined || arg2 === undefined) {
+    failed= arg1.concat(" is ", arg2);
+  console.log(failed);
+} else {
+    result= arg1.concat(" is ", arg2)
+  console.log(result);
+}
