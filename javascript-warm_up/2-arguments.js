40e3c1b4ca1edec9cb7a781f0dd944787831eead
User10538 <10538@holbertonstudents.com>
2025-07-16 13:34:10 +1000

    fix error

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 
-function checkForArgument (...arg) {
-  if (arg.length === 0) {
-    console.log('No argument');
-  } else if (arg.length === 1) {
-    console.log('Argument found');
-  } else {
-    console.log('Argument found');
-  }
+const arg = process.argv.slice(2);
+if (arg.length === 0) {
+  console.log('No argument');
+} else if (arg.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-
-// Example usage:
-checkForArgument(); // No argument
-checkForArgument('test'); // Argument found
-checkForArgument(1, 2, 3); // Arguments found
