dbaa15be7b1fd38c894e07b243143f00955e70ee
User10538 <10538@holbertonstudents.com>
2025-07-16 15:43:00 +1000

    fix error

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const arg = process.argv.slice(2);
-if (arg[0] === undefined) {
+const arg = process.argv[2];
+if (arg === undefined) {
   console.log('No argument');
 } else {
   console.log(arg);
