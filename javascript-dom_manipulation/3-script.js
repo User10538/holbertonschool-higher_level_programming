91aca50ab2f3a081cf9be8b41040bb12038112e3
User10538 <10538@holbertonstudents.com>
2025-07-18 00:04:20 +1000

    Toggle colour

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('toggle_header').addEventListener('click', function () {
+    const header = document.querySelector('header');
+    if (header.classList.contains('red')) {
+      header.classList.remove('red');
+      header.classList.add('green');
+    } else {
+      header.classList.remove('green');
+      header.classList.add('red');
+    }
+  });
+});
