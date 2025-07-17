9115db465e45f0f1bf0ecf618b1e61253fc7ad3d
User10538 <10538@holbertonstudents.com>
2025-07-18 02:02:30 +1000

    Get movie title using getEach

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const movies = data.results;
+      const ul = document.getElementById('list_movies');
+
+      movies.forEach(function (movies) {
+        // to add li to ul
+        const li = document.createElement('li');
+        li.textContent = movies.title;
+        ul.appendChild(li);
+      });
+    });
+});
