6c0b711e4c2c826bbf337a7f49568f685046185e
User10538 <10538@holbertonstudents.com>
2025-07-22 22:40:45 +1000

    Fix error

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -83,7 +83,8 @@ def create_database():
                    ''')
     conn.commit()
     conn.close()
-    if __name__ == '__main__':
-        create_database()
-        app.run(debug=True, port=5000)    
+    
+if __name__ == '__main__':
+    create_database()
+    app.run(debug=True, port=5000)    
         
