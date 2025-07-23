90de0dc2ccfe0036fd0ba28d671070dbdfd21e4e
User10538 <10538@holbertonstudents.com>
2025-07-23 15:18:44 +1000

    Fix error

--- python-server_side_rendering/products.sql
+++ python-server_side_rendering/products.sql
@@ -8,4 +8,3 @@ CREATE TABLE IF NOT EXISTS Products (
     category VARCHAR(256) NOT NULL,
     price DECIMAL(10, 2) NOT NULL
 );
-INSERT INTO products (name, category, price) VALUES ('Jarvis', 'AI', 9999.99);
