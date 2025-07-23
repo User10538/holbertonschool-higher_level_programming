b2f62d566ba320aa6ebe501cc4ed456dfbcdb2c0
User10538 <10538@holbertonstudents.com>
2025-07-23 15:33:16 +1000

    Fix error

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -27,20 +27,20 @@ def items():
 @app.route('/products')
 def products():
     data = []
-    matrix = []
+    get_id = []
     if request.args.get('source') == 'json':
         with open('products.json', 'r') as f:
             data = json.load(f)
         if request.args.get('id'):
-            matrix = [row for row in data if row['id'] == int(request.args.get('id'))]
+            get_id = [row for row in data if row['id'] == int(request.args.get('id'))]
         else:
-            matrix = data
+            get_id = data
     elif request.args.get('source') == 'csv':
         with open('products.csv', mode='r', newline='') as f:
             data = csv.DictReader(f)
-            matrix = [row for row in data]
+            get_id = [row for row in data]
         if request.args.get('id'):
-            matrix = [row for row in matrix if row['id'] == request.args.get('id')]
+            get_id = [row for row in get_id if row['id'] == request.args.get('id')]
     elif request.args.get('source') == 'sql':
         conn = sqlite3.connect('products.db')
         conn.row_factory = sqlite3.Row
@@ -53,13 +53,13 @@ def products():
             ''')
         res = cursor.fetchall()
         fields = [column[0] for column in cursor.description]
-        matrix = [{key: value for key, value in zip(fields, row)} for row in res]
+        get_id = [{key: value for key, value in zip(fields, row)} for row in res]
         conn.close()
     else:
-        matrix = 'error'
-    if len(matrix) == 0:
-        matrix = 'no product'
-    return render_template('product_display.html', data=matrix)
+        get_id = 'error'
+    if len(get_id) == 0:
+        get_id = 'no product'
+    return render_template('product_display.html', data=get_id)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
