d6d1fd76858915297263c1ef19e0e48b002237ed
User10538 <10538@holbertonstudents.com>
2025-07-23 14:09:40 +1000

    fix error

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -101,7 +101,8 @@ def create_database():
                    (1, 'Laptop', 'Electronics', 799.99),
                    (2, 'Coffee Mug', 'Home Goods', 15.99),
                    (3, 'Tim', 'AI Assistant', 2999.99),
-                   (4, 'Tesla Coil', 'Electronics', 499.99)
+                   (4, 'Tesla Coil', 'Electronics', 499.99),
+                   (5, 'Jarvis', 'AI', 9999.99)
                    ''')
     conn.commit()
     conn.close()
